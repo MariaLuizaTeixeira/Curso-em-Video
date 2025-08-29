4a4e6ab1929b5f8a3b29f5e80421136e54dd24d4
Maria Luíza Ramos Teixeira <marialuizat.ifpr2024@gmail.com>
2025-08-29 17:44:23 -0300

    comecei sql

---
+++ pacote-download/banco_de_dados/aula01.sql
@@ -0,0 +1,12 @@
+CREATE DATABASE cadastro;
+use cadastro;
+CREATE TABLE pessoas (
+nome varchar (30),
+idade tinyint,
+sexo char(1),
+altura float,
+peso float,
+nacionalidade varchar(20)
+);
+
+describe pessoas;
