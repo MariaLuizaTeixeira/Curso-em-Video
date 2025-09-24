a1954627066ca2352dff51a6f030b7b5ea7fa4b6
Maria Luíza Ramos Teixeira <marialuizat.ifpr2024@gmail.com>
2025-09-24 17:44:53 -0300

    continuando python - funções 2

---
+++ pacote-download/python/modulo03/aula05.py
@@ -0,0 +1,30 @@
+help(print)
+print(input.__doc__)
+
+def contador(i, f, p):
+    """
+    Faz uma contagem e mostra na tela.
+    :param i: início da contagem
+    :param f: fim da contagem
+    :param p: passo da contagem
+    :return: sem retorno
+    """
+    c = i
+    while c <= f:
+        print(f'{c} ', end='')
+        c += p
+    print('FIM!')
+help(contador)
+
+def somar(a=0, b=0, c=0):
+    """
+    Soma três valores e mostra o resultado na tela.
+    :param a: o primeiro valor
+    :param b: o segundo valor
+    :param c: o terceiro valor
+    :return: sem retorno
+    """
+    s = a + b + c
+    print(f'A soma vale {s}')
+somar(3, 2, 5)
+somar(8, 4)
